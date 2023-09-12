b1b52196483167ecadcde082a543fa7eb30033c2
vtsaijagadishgit <saijagadish555@gmail.com>
2023-09-12 10:58:29 +0200

    Update main.tf

--- platform/main.tf
+++ platform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cdaas"
+    bucket         = "cdaas-vfz"
     key            = "terraform.tfstate"
     region         = "eu-central-1"    
     encrypt        = true
