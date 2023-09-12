6b02972399cfddfbf77c25c4f087d84ce001db56
vtsaijagadishgit <saijagadish555@gmail.com>
2023-09-12 11:49:41 +0200

    Update output.tf

--- platform/output.tf
+++ platform/output.tf
@@ -1,7 +1,4 @@
-output "CDAAS1_public_ip" {
-  value = module.EC2.instance_public_ips[0]
-}
-
-output "CDAAS2_public_ip" {
-  value = module.EC2.instance_public_ips[1]
+output "instance_ids" {
+  description = "List of instance IDs"
+  value       = module.ec2_instance.instance_ids
 }
