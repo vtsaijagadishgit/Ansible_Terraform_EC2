91080415ab597ae3b307207d9df91ec944687ee2
vtsaijagadishgit <saijagadish555@gmail.com>
2023-09-12 10:47:36 +0200

    Create userdata.sh

---
+++ platform/ec2/userdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update -y
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ubuntu
