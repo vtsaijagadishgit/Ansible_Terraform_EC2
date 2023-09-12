28d5dad60fb4fb0f45b494b99d3cbaf0875abd0e
vtsaijagadishgit <saijagadish555@gmail.com>
2023-09-12 11:13:47 +0200

    Update output.tf

--- platform/output.tf
+++ platform/output.tf
@@ -1,4 +1,4 @@
 output "CDAAS_public_ip" {
-  value = aws_instance.CDAAS[0].public_ip
-  value = aws_instance.CDAAS[1].public_ip
+  value = module.EC2.CDAAS[0].public_ip
+  value = module.EC2.CDAAS[1].public_ip
 }
