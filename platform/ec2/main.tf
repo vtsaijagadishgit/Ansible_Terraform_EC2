d18dab699fbfe594d10a9cd48765d82872c8ebf9
vtsaijagadishgit <saijagadish555@gmail.com>
2023-09-12 10:44:01 +0200

    Create main.tf

---
+++ platform/ec2/main.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "App-Server" {
+  ami           = "ami-04e601abe3e1a910f" 
+  instance_type = "t2.micro"            
+
+  vpc_security_group_ids = [aws_security_group.app-server-SG.id]
+  subnet_id              = "subnet-38325c52"
+  key_name               = "pfops"
+  associate_public_ip_address = true
+  user_data = file("./modules/EC2/userdata.sh")
+
+  tags = {
+    Name = "App-server"
+  }
+}
+
+resource "aws_security_group" "app-server-SG" {
+  name        = "app-server-SG"
+  description = "Security group for the app-server-SG"
+
+  vpc_id = "vpc-4b4bcc21"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # Allow SSH access from anywhere
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # Allow 8080 access from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]   # Allow all outbound traffic
+  }
+}
